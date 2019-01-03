ae864f3850e2513d8a4c44c8f4d50141cf669076
qmzphu <cxcongxin@gmail.com>
2019-01-03 15:21:33 +0800

    delete commits and create new function of load data

--- DataLoader.py
+++ DataLoader.py
@@ -44,44 +44,36 @@ def create_data(source_sents, target_sents):
     
     return X, Y, Sources, Targets
 
-def load_train_data():
-    src_sents = [regex.sub("[^\s\p{Han}']", "", line) for line in codecs.open(hp.source_train, "r", "utf-8").read().split("\n") if line and line[0] != "<"]
-    trgt_sents = [regex.sub("[^\s\p{Latin}']", "", line) for line in codecs.open(hp.target_train, "r", "utf-8").read().split("\n") if line and line[0] != "<"]
+def load_data(src_file, trgt_file):
+    src_sents = [regex.sub("[^\s\p{Han}']", "", line) for line in codecs.open(src_file, "r", "utf-8").read().split("\n") if line and line[0] != "<"]
+    trgt_sents = [regex.sub("[^\s\p{Latin}']", "", line) for line in codecs.open(trgt_file, "r", "utf-8").read().split("\n") if line and line[0] != "<"]
     X, Y, Sources, Targets = create_data(src_sents, trgt_sents)
     return X, Y
+
+def load_train_data():
+    return load_data(hp.source_train, hp.target_train)
+#     src_sents = [regex.sub("[^\s\p{Han}']", "", line) for line in codecs.open(hp.source_train, "r", "utf-8").read().split("\n") if line and line[0] != "<"]
+#     trgt_sents = [regex.sub("[^\s\p{Latin}']", "", line) for line in codecs.open(hp.target_train, "r", "utf-8").read().split("\n") if line and line[0] != "<"]
+#     X, Y, Sources, Targets = create_data(src_sents, trgt_sents)
+#     return X, Y
     
 def load_test_data():
-#     def _refine1(line):
-#         line = regex.sub("[^\s\p{Han}']", "", line) 
-#         return line.strip()
-# 
-#     def _refine2(line):
-#         line = regex.sub("[^\s\p{Latin}']", "", line) 
-#         return line.strip()
-#     
-#     src_sents = [_refine1(line) for line in codecs.open(hp.source_test, "r", "utf-8").read().split("\n")]
-#     trgt_sents = [_refine2(line) for line in codecs.open(hp.target_test, "r", "utf-8").read().split("\n")]
-    src_sents = [regex.sub("[^\s\p{Han}']", "", line).strip() for line in codecs.open(hp.source_test, "r", "utf-8").read().split("\n")]
-    trgt_sents = [regex.sub("[^\s\p{Latin}']", "", line).strip() for line in codecs.open(hp.target_test, "r", "utf-8").read().split("\n")]
-        
-    X, Y, Sources, Targets = create_data(src_sents, trgt_sents)
-    return X, Sources, Targets # (1064, 150)
+    return load_data(hp.source_test, hp.target_test)
+#     src_sents = [regex.sub("[^\s\p{Han}']", "", line).strip() for line in codecs.open(hp.source_test, "r", "utf-8").read().split("\n")]
+#     trgt_sents = [regex.sub("[^\s\p{Latin}']", "", line).strip() for line in codecs.open(hp.target_test, "r", "utf-8").read().split("\n")]
+#     X, Y, Sources, Targets = create_data(src_sents, trgt_sents)
+#     return X, Sources, Targets # (1064, 150)
 
 def get_batch_data():
-    # Load data
     X, Y = load_train_data()
     
-    # calc total batch count
     num_batch = len(X) // hp.batch_size
     
-    # Convert to tensor
     X = tf.convert_to_tensor(X, tf.int32)
     Y = tf.convert_to_tensor(Y, tf.int32)
     
-    # Create Queues
     input_queues = tf.train.slice_input_producer([X, Y])
             
-    # create batch queues
     x, y = tf.train.shuffle_batch(input_queues,
                                 num_threads=8,
                                 batch_size=hp.batch_size, 
@@ -89,5 +81,5 @@ def get_batch_data():
                                 min_after_dequeue=hp.batch_size*32, 
                                 allow_smaller_final_batch=False)
     
-    return x, y, num_batch # (N, T), (N, T), ()
+    return x, y, num_batch
 
